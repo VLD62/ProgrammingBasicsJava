e1507de99c1b0754bca40e3c33c947e53915f825
VLD62 <iliev62@gmail.com>
2017-04-22 11:34:56 +0300

    Adding tasks from Lecture4

--- First-Steps-in-Coding/src/Main.java
+++ First-Steps-in-Coding/src/Main.java
@@ -2,7 +2,7 @@
  * Created by vld62 on 4/22/17.
  */
 public class Main {
-    public static void main (String [] Args) {
+    public static void main (String [] args) {
         System.out.println("Hello Java");
     }
 }
