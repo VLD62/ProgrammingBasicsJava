e0ed1576db6dadf8dcd8233cdacc1ce19b4bed68
VLD62 <iliev62@gmail.com>
2017-04-29 13:57:09 +0300

    all mandatory tasks for Lecture5

--- 02Simple-Calculations/src/p13_1000DaysAfterBirth.java
+++ 02Simple-Calculations/src/p13_1000DaysAfterBirth.java
@@ -1,5 +1,16 @@
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+import java.time.LocalDate;
 /**
  * Created by vld62 on 4/29/17.
  */
 public class p13_1000DaysAfterBirth {
+    public static void main(String[] args) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        Scanner in = new Scanner(System.in);
+        String date = in.nextLine();
+        LocalDate dateFormatted = LocalDate.parse(date, formatter);
+        LocalDate outDate = dateFormatted.plusDays(999);
+        System.out.println(outDate.format(formatter));
+    }
 }
