01a556bf205da551140319acd90e17d72bdda65c
unknown <viliev@VILIEV.dbdc.luxoft.com>
2017-05-15 19:00:26 +0300

    adding tasks for lecture 6

---
+++ 03Simple-Conditions/src/TransportPrice.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+/**
+ * Created by viliev on 15.5.2017 г..
+ */
+public class TransportPrice {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+    }
+}
