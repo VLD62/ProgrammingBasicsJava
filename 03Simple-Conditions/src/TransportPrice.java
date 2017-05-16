3030ffbce3e4f3388efd7b373cae24946a411777
unknown <viliev@VILIEV.dbdc.luxoft.com>
2017-05-16 12:23:13 +0300

    adding tasks from lecture 6

--- 03Simple-Conditions/src/TransportPrice.java
+++ 03Simple-Conditions/src/TransportPrice.java
@@ -6,6 +6,29 @@ import java.util.Scanner;
 public class TransportPrice {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+        int n = Integer.parseInt(in.nextLine());
+        String period = in.nextLine();
+        double totalCost = 0;
+        if (n > 0 && n <= 5000) {
+            if (n < 20) {
+                if (period.equals("day")) {
+                    totalCost = 0.7+0.79*n;
+                } else if (period.equals("night")) {
+                    totalCost = 0.7+0.90*n;
+                } else {
+                    System.out.println("unknown period");
+                }
+            } else {
+                if (n < 100) {
+                    totalCost = 0.09*n;
+                } else {
+                    totalCost = 0.06*n;
+                }
+            }
+            System.out.printf("%.2f",totalCost);
+
+        } else {
+            System.out.println("Distance can be between 0 and 5000 km only!");
+        }
     }
 }
