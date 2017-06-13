470f1db9576acd548892ce3189bc40d36f0cb638
unknown <viliev@VILIEV.dbdc.luxoft.com>
2017-06-13 19:32:34 +0300

    adding tasks from lecture 8

--- 07Advanced-Loops/src/GreatestCommonDivisor.java
+++ 07Advanced-Loops/src/GreatestCommonDivisor.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 /**
  * Created by viliev on 13.6.2017 г..
  */
 public class GreatestCommonDivisor {
-    
+    Scanner in = new Scanner(System.in);
+
+    int n = Integer.parseInt(in.nextLine());
+    int m = Integer.parseInt(in.nextLine());
 }
